31f4d8ddcc0f896395ccec556305ff2145c458f3
bailey0505 <baileyrotellini1998@gmail.com>
2019-11-04 16:25:24 -0500

    mall implementations, starting on getting the register page up and running

--- controller.js
+++ controller.js
@@ -1,12 +1,11 @@
 /****************************
-Router For Point Park Uno Club Website App 
 
-Created by: Bailey, Gavin, and Tony
+Router For Point Park Uno Club Website App 
 
 *****************************/
 
 
-
+//Required package for project 
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var bodyParser = require('body-parser');
@@ -15,12 +14,13 @@ var mysql = require('mysql');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var path = require('path');
+var validator = require('validator');
 
 
 
 
 
-
+//Required Files that give us credentials and other data
 var modules = require('./lib/modules.js');
 var login = require('./framework/login.js');
 var register = require('./framework/register.js');
@@ -54,24 +54,7 @@ app.use(express.urlencoded());
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-   
-/*
-var conn = mysql.createConnection(sqlcredentials.connection);
-conn.connect(function(err) {
-  if (err) {
-    console.error("ERROR: cannot connect: " + err);
-    return;
-  }
-  conn.query("SELECT * FROM users", function(err, rows, fields) {
-    if (err) {
-      console.error("ERROR: query failed: " + err);
-      return;
-    }
-    console.log(JSON.stringify(rows));
-  });
-  conn.end();
-});
-*/
+ 
 
 app.set('port', process.env.PORT || 3000);
 
@@ -103,13 +86,15 @@ app.get('/rules', function(req, res) {
 
 });
 app.get('/login', function(req, res) {
- res.render('login', {
+    res.render('login', {
      incorrect:  req.session.incoorect
  });
 
 });
 app.get('/register', function(req, res) {
- res.render('register');
+ res.render('register', {
+     incorrect_email :  req.session.incorrect_email
+ });
 
 });
 app.get('/logout', function(req, res) {
@@ -118,22 +103,26 @@ app.get('/logout', function(req, res) {
 
 });
 
+//Authenticating user login, written by bailey 
 app.post('/auth', (req, res) => {
-    //Authenticating user login
+    //Grabbing Page vars
     const username = req.body.username;
     const password = req.body.password;
     
+    //establishing sql connect using outside var for creds
    var conn = mysql.createConnection(sqlcredentials.connection);
     conn.connect(function(err) {
     if (err) {
         console.error("ERROR: cannot connect: " + err);
         return;
-    }
+    }  
+      //creating sql var for query, query grabs the username and passoword from sql if it is equal to user input 
       var sql = mysql.format("SELECT * FROM users WHERE username=? AND password=?", [username, password]);
-      console.log(sql);
-    
+      //test log to see what the qeury will be 
+      //console.log(sql);
+        
         conn.query(sql, function(err, results, fields) {
-      
+        //query checks if the user actually exists with that password, if yes sets the session vars and redirects them back to home page
         if(results.length > 0) {
             console.log('success if');
             req.session.loggedin = 'true';
@@ -142,7 +131,7 @@ app.post('/auth', (req, res) => {
             res.redirect('/');
             
         }else{
-         req.session.loggedin = false;
+        //happens if user has wrong password or username, activates the pop up for incorrect username or password
          req.session.username = '';
          req.session.incoorect = 'on';
          res.redirect('/login');
@@ -151,6 +140,34 @@ app.post('/auth', (req, res) => {
   });
     conn.end();
 });
+    
+});
+
+//Adding new user, written by bailey 
+app.post('/registeruser?', (req, res) =>{
+    //Grabbing Variables from page
+    var name =  req.body.name;
+    var email =  req.body.email;
+    var username =  req.body.username;
+    var password =  req.body.password;
+    var password_retype = req.body.passwordr;
+    //test to see if variables were coming through
+    //console.log(name + email + username + password + password_retype);
+    
+    //Now to process the variables to see if they are what we need. Going to check if email is an actual email and if password retype is correct 
+  if(validator.isEmail(email) === false) {
+      //activates session var Improper email
+      req.session.incorrect_email = "on"; 
+      res.redirect('/register');
+  } else{
+      //deletes incorrect_email session var on second try if they do enter a correct one 
+     delete req.session.incorrect_email; 
+    }
+    
+   
+    
+    
+    
     
 });
 
---
+++ readme.txt
@@ -0,0 +1,11 @@
+This File outlines how to use this application.
+
+The file to run the server is controller.js 
+
+The Application is going to be for the Point Park Uno Club adn will feature user login, stats and other things to be determined. 
+
+*** NOTICE *****
+We are not going to be pushing the videos, framework , and node_modules to git. Video files are to large for the git repository and we will figure out a solution for live implementation. The framework folder will hold all of your credential files and can be implemented and routes changes via the controller. The node_modules clearly do not get pushed since you can init npm to get themauto downloaded from the package. 
+
+
+
